431c3344ff9f2a97bb8949ddf79046eec421598d
sdn3cicurug71-beep <sdn3cicurug.71@gmail.com>
2026-06-01 12:05:16 +0700

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycby90pbrcatRjBmEdx1w-BQ_ZVbQO1aiJvwdIV8x3x5jRX0XCnd5wa3INYlXIsNSrdwdfQ/exec"; 
 
 export interface FormField {
   id: string;
